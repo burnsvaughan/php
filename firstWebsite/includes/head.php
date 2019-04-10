0231cdae8a304663968418da272f509b94ff97fa
Burns Vaughan <47879471+burnsvaughan@users.noreply.github.com>
2019-04-11 02:00:06 +1200

    Add files via upload

---
+++ firstWebsite/includes/head.php
@@ -0,0 +1,18 @@
+<style>
+                .container{
+                    max-width:800px;
+                    margin: 0 auto;
+                    text-align: center;
+                    text-decoration: none;
+                    font-size:4rem;
+                }
+                .container a{
+                    text-decoration: none;
+                    color: black;
+                    font-size:2rem;
+                }
+                .container a.active{
+                    background: red;
+                }
+                
+            </style>
---
+++ firstWebsite/includes/topscript.php
@@ -0,0 +1,2 @@
+<?php $location = $_SERVER['REQUEST_URI'];
+   echo $_SERVER["REQUEST_URI"]; ?>
