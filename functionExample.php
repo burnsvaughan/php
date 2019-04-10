fb96915d43ecc4f10df226d9fcc5d54cb5c5c1d4
Burns Vaughan <47879471+burnsvaughan@users.noreply.github.com>
2019-04-10 17:52:25 +1200

    Create functionExample.php
    takes two parameters, number of times to print, and a string.
    writes the string the number of times to the screen.

---
+++ functionExample.php
@@ -0,0 +1,21 @@
+    <?php
+
+    $i = 0;
+
+    function writeSentence($a, $b) {
+        for ($i = 0; $i < $a; $i++) {
+            echo $b;
+        }
+    }
+    writeSentence(5, "derp a derp ");
+
+    ?>
+
+
+    <html>
+
+    <body>
+
+    </body>
+
+    </html>
