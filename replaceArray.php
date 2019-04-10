8aacbabc2977985794d8e2dedcc37f2c34dec5c5
Burns Vaughan <47879471+burnsvaughan@users.noreply.github.com>
2019-04-10 16:17:16 +1200

    Create replaceArray.php

---
+++ replaceArray.php
@@ -0,0 +1,42 @@
+<?php
+
+$cars = [
+            ["brand" => "BMW",
+            "make" => "i300",
+            "wheels" => "4",
+            "doors" => "5"],
+
+        ["brand" => "Mercedes",
+        "make" => "Benz",
+        "wheels" => "4",
+        "doors" => "4"],
+
+        ["brand" => "Toyota",
+        "make" => "Corolla",
+        "wheels" => "5",
+        "doors" => "5"],
+
+        ["brand" => "Mazda",
+        "make" => "CRX",
+        "wheels" => "4",
+        "doors" => "2"],
+    ];
+
+        $cars[0]["doors"] = 2;
+
+
+
+    echo $cars[2]["wheels"];
+    echo $cars[0]["doors"];
+
+?>
+
+
+<html>
+
+<body>
+
+</body>
+
+</html>
+
