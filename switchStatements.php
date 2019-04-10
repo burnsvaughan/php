52997b8fb991a148158cd4983aba783023d432f2
Burns Vaughan <47879471+burnsvaughan@users.noreply.github.com>
2019-04-10 13:29:40 +1200

    Create switchStatements.php

---
+++ switchStatements.php
@@ -0,0 +1,28 @@
+<?php
+
+$moneyOwed = "60";
+
+# hi there don't touch anything thanks
+
+switch ($moneyOwed) {
+        case $moneyOwed > 50 && $moneyOwed < 100:
+        echo "Have a free soda";
+    break;
+        case $moneyOwed > 100:
+        echo "Have a free soda and a t-bone steak";
+    break;
+    default:
+    echo "The total price for today is {$moneyOwed}";
+}
+
+?>
+
+
+<html>
+
+<body>
+
+</body>
+
+</html>
+
