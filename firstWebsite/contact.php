f9980c0222b6b7a11b2b32175c06a17d1df1d4a7
Burns Vaughan <47879471+burnsvaughan@users.noreply.github.com>
2019-04-11 01:59:01 +1200

    Add files via upload
    php files

---
+++ firstWebsite/contact.php
@@ -0,0 +1,23 @@
+    <?php
+
+   require('./includes/topscript.php');
+
+    ?>
+
+
+    <html>
+    <head></head>
+
+    <body>
+        <div class="container">
+            <?php include('includes/header.php') ?>
+            <?php require('includes/head.php') ?>
+          
+            <section>
+                <h1>Contact</h1>
+            </section>
+        </div>
+
+    </body>
+
+    </html>
---
+++ firstWebsite/index.php
@@ -0,0 +1,22 @@
+    <?php
+
+   require('./includes/topscript.php');
+    ?>
+
+
+    <html>
+
+    <body>
+        <?php require('includes/head.php') ?>
+        <div class="container">
+            <?php include('includes/header.php') ?>
+            
+          
+            <section>
+                <h1>HomePage</h1>
+            </section>
+        </div>
+
+    </body>
+
+    </html>
